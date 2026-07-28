7f2c4890631901fd3bcb57c138aafe63b165d465
abd.rifai <rhifay@gmail.com>
2026-07-28 17:56:56 +0800

    refactor: replace deprecated csrf.checkOrigin with csrf.trustedOrigins

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		csrf: {
-			checkOrigin: false,
+			trustedOrigins: ['http://33.33.33.5', 'http://36.92.162.234:8585', 'http://localhost:5173', 'http://localhost:3001']
 		}
 	}
 };
